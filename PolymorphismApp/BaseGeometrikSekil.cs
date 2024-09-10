57b6d5df779baacbcffe083ff5363b63e4a826b0
KAAN <alikaanozoll@gmail.com>
2024-09-10 18:55:47 +0300

    Add project files.

---
+++ PolymorphismApp/BaseGeometrikSekil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismApp
+{
+    public class BaseGeometrikSekil   // BaseGeometrikSekil nesnelerinin tanımlanması ||Identifying BaseGeometricShape objects
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+    public virtual void CalculateArea()
+        {
+           int area= Width * Height;
+            Console.WriteLine($"Alan : {area}");
+        }
+    }
+
+    public class Square : BaseGeometrikSekil 
+    {
+        public override void CalculateArea()
+        {
+            base.CalculateArea();
+        }
+    }
+    public class Rectangle : BaseGeometrikSekil
+    {
+        public override void CalculateArea()
+        {
+            base.CalculateArea();
+        }
+    }
+
+    public class RightTriangle : BaseGeometrikSekil
+    {
+        public override void CalculateArea()
+        {
+            int area = (Width * Height) / 2;
+            Console.WriteLine($"Alan : {area}");
+
+        }
+    }
+
+}
---
+++ PolymorphismApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using PolymorphismApp;
+
+Square square=new Square();
+square.Width = 100;
+square.Height = 100;
+square.CalculateArea();
+
+Rectangle rectangle=new Rectangle();
+rectangle.Width = 250;
+rectangle.Height = 100;
+rectangle.CalculateArea();
+
+RightTriangle rightTriangle=new RightTriangle();
+rightTriangle.Width = 90;
+rightTriangle.Height = 100;
+rightTriangle.CalculateArea();
